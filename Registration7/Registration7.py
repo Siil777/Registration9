ca9efaf99041e2ad6d8c355b70c1022c88ffad8b
Siil777 <118188578+Siil777@users.noreply.github.com>
2023-01-21 18:47:54 +0200

    Update Registration7.py

--- Registration7/Registration7.py
+++ Registration7/Registration7.py
@@ -3,11 +3,14 @@ from module1 import*
 import sys
 import string
 from re import*
+from module1 import get_data
 
 while True:
-        print("Write 1 for reg or 0 for exit")
+        print("Write 1 for reg, 2 for login, or 0 for exit")
         cmd = input()
         if cmd == '1':
+            add_user=()
+        elif cmd=='2':
             add_user=()
         elif cmd == '0': 
             sys.exit()
@@ -16,8 +19,16 @@ while True:
         while True:
                 login = input("Enter login: ")
                 passw = input("Enter password: ")
-                if login not in logins:
+                if login not in logins and cmd=='1':
+
                     print("Success.")
-                    users[login] = passw
+                    break
+                if login in logins and cmd=='2':
+                    print('you logened')
+                    
+                    break
+                if login not in logins and cmd=='2':
+                    print('you ought to register for start')
                     break
                 else: print("There is a user with this username. Try again with other username.")
+                break
